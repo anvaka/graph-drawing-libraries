27942193ec82d26a3b3bc9ed41de3ed166849b21
anvaka <anvaka@gmail.com>
2014-07-05 12:14:23 -0700

    Added offline data
    Offline data will be autogenerated during release time and will let
    users without github account see statistics.

--- src/src/scripts/libraries.js
+++ src/src/scripts/libraries.js
@@ -1,10 +1,13 @@
 var moment = require('moment');
+var offlineData = require('./offlineData');
 
 module.exports = function getLibraries($http, $q, libraries) {
   return $q.all(libraries.map(getRepositoryInfo));
 
   function getRepositoryInfo(name) {
-    return $http.get('https://api.github.com/repos/' + name).then(extractFields);
+    return $http.get('https://api.github.com/repos/' + name)
+                .then(extractFields)
+                .then(mergeWithOfflineData);
   }
 
   function extractFields(reposResponse) {
@@ -23,5 +26,21 @@ module.exports = function getLibraries($http, $q, libraries) {
       updatedTooltip: repoInfo.pushed_at,
       commits: 'tbd'
     };
+  }
+
+  function mergeWithOfflineData(repoInfo) {
+    var offlineInfo = offlineData.libraries[repoInfo.name];
+    if (!offlineInfo) {
+      // we don't know wabout this library, bail out:
+      return repoInfo;
+    }
+    for (var name in offlineInfo) {
+      // skip standard properties and those which are present in the online data:
+      if (!offlineInfo.hasOwnProperty(name) || repoInfo.hasOwnProperty(name)) continue;
+
+      repoInfo[name] = offlineInfo[name];
+    }
+
+    return repoInfo;
   }
 };
