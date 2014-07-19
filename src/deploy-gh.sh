8199073e28528b0fe14a82227ef64a044d896d3e
anvaka <anvaka@gmail.com>
2014-07-18 21:12:53 -0700

    properly setting prod build flag

--- src/deploy-gh.sh
+++ src/deploy-gh.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 rm -rf dist || exit 0;
 mkdir dist;
-env PROD_BUILD=1 && gulp build
+PROD_BUILD=1 && export PROD_BUILD && gulp build
 ( cd dist
  git init
  git add .
  git commit -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@github.com/anvaka/graph-drawing-libraries" master:gh-pages > /dev/null 2>&1
 )
+unset PROD_BUILD
