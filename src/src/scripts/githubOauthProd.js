3d613b7df519d08e06f487887fbe1eeadef8257b
anvaka <anvaka@gmail.com>
2014-07-18 21:16:01 -0700

    Have to use square brakets

--- src/src/scripts/githubOauthProd.js
+++ src/src/scripts/githubOauthProd.js
@@ -1,4 +1,4 @@
 module.exports = {
   clientId: 'cbd8a7461321510865d9',
-  oauthProxy: '//ghoauth.herokuapp.com/authenticate/{{code}}?case=graph-prod'
+  oauthProxy: '//ghoauth.herokuapp.com/authenticate/[code]?case=graph-prod'
 };
