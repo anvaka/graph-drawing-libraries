eb6c384aa0d53ded0a91b89fca6c159429a5ae83
anvaka <anvaka@gmail.com>
2014-08-08 09:20:58 -0700

    Added links to basic examples

--- src/src/scripts/library/libraryDetailsController.js
+++ src/src/scripts/library/libraryDetailsController.js
@@ -1,7 +1,10 @@
 require('an').controller(LibraryDetailsController, 'LibraryDetailsController');
 
 function LibraryDetailsController($scope, $routeParams) {
-  $scope.name = $routeParams.libraryName.replace('_', '/');
+  var libraryName = $routeParams.libraryName;
+  $scope.name = libraryName.replace('_', '/');
+  $scope.basicExampleUrl = '../examples/' + libraryName + '/01.basic/';
+  $scope.basicExampleSrcUrl = 'https://github.com/anvaka/graph-drawing-libraries/tree/master/src/src/scripts/examples/' + libraryName + '/01.basic/';
 }
 
 LibraryDetailsController.$inject = ['$scope', '$routeParams'];
