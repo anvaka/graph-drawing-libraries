c37fb640d225ec38758b72b8a872e60ff52d5e09
anvaka <anvaka@gmail.com>
2014-08-09 12:58:08 -0700

    Exclude node_modules from watching

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -14,7 +14,7 @@ var devServer = {
 };
 
 var paths = {
-  scripts: ['src/**/*.*'],
+  scripts: ['src/**/*.*', '!src/**/node_modules/*.*'],
   markup: ['src/*.html'],
   styles: { paths: [ path.join(__dirname, 'src/styles') ] }
 };
