e92ca80fb03f29d11e90ef580e31815b4083fb1c
anvaka <anvaka@gmail.com>
2014-08-23 19:43:52 -0700

    Moved share to a separate module

--- src/src/scripts/navController.js
+++ src/src/scripts/navController.js
@@ -1,27 +1,9 @@
 module.exports = require('an').controller('navController', navController);
 
 function navController($scope, $location) {
-  var services = {
-    twitter: function (url) {
-      return 'https://twitter.com/intent/tweet?url=' + decodeURIComponent(url).replace(/#/g, '%23');
-    },
-    google: function (url) {
-      return 'https://plus.google.com/share?url=' + url;
-    },
-    facebook: function (url) {
-      return 'http://www.facebook.com/sharer/sharer.php?u=' + url;
-    }
-  };
-
-  $scope.share = share;
+  $scope.share = require('./share');
   $scope.$on('$locationChangeSuccess', locationChanged);
   $scope.lastDetailsRoute = '#/library/almende_vis';
-
-  function share(serviceName, e) {
-    var url = encodeURIComponent($location.absUrl());
-    window.open(services[serviceName](url), 'Share' + serviceName, 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=420,width=600');
-    e.preventDefault();
-  }
 
   function locationChanged(e, newRoute, oldRoute) {
     var libraryIdx = newRoute.indexOf('#/library/');
