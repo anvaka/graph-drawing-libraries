d5feb3f06f30747e3fe175c396e0dd42c6188ab7
anvaka <anvaka@gmail.com>
2014-08-25 19:05:02 -0700

    Added progress stats

--- src/src/scripts/all/allController.js
+++ src/src/scripts/all/allController.js
@@ -3,36 +3,43 @@ require('an').controller(AllController, 'AllController');
 
 var libraries;
 
-function AllController($scope, $http, $location) {
+function AllController($scope, $http, $location, $timeout) {
   var getLibraries = require('./libraries');
+  var timeouts = [];
 
   if (!libraries) {
-    getLibraries($http).then(initLibraries);
+    getLibraries($http).then(initLibraries, showError);
+
+    timeouts = [
+      $timeout(function() { if (!libraries) $scope.showSecondLoading = true; }, 2000),
+      $timeout(function() { if (!libraries) $scope.showThirdLoading = true; }, 6000),
+      $timeout(showError, 14000)
+    ];
   } else {
     initLibraries(libraries);
   }
 
-  $scope.routeTo = function (library) {
+  $scope.routeTo = function(library) {
     $location.path(getRoute(library));
   };
 
-  $scope.getRoute = getRoute; 
+  $scope.getRoute = getRoute;
 
   $scope.sort = {
     name: 'name',
     direction: 1
   };
 
-  $scope.sortBy = function (name) {
+  $scope.sortBy = function(name) {
     var sort = $scope.sort;
     if (sort.name === name) sort.direction *= -1;
     else sort.direction = 1;
 
     sort.name = name;
 
-    $scope.libraries.sort(function (a, b) {
+    $scope.libraries.sort(function(a, b) {
       return a[name] < b[name] ? 1 * sort.direction :
-             a[name] === b[name] ? 0 : -1 * sort.direction;
+        a[name] === b[name] ? 0 : -1 * sort.direction;
     });
   };
 
@@ -43,8 +50,17 @@ function AllController($scope, $http, $location) {
   function initLibraries(foundLibraries) {
     $scope.libraries = foundLibraries;
     $scope.loaded = true;
+    $scope.showSecondLoading = false;
+    $scope.showThirdLoading = false;
+    $scope.showErrorLoading = false;
+
     libraries = foundLibraries;
+  }
+
+  function showError() {
+    if (!libraries) $scope.showErrorLoading = true;
+    timeouts.forEach($timeout.cancel);
   }
 }
 
-AllController.$inject = ['$scope', '$http', '$location'];
+AllController.$inject = ['$scope', '$http', '$location', '$timeout'];
